f330d2dcc15aee3ebb9bb48cb776e5ba5634c176
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-01-31 14:27:04 +0100

    Test: Add test for UserInformation, ref #4

---
+++ test/unit/helpers/UserInformation.spec.js
@@ -0,0 +1,17 @@
+import UserInformation from 'Helpers/UserInformation';
+
+describe('The UserInformation helper', () => {
+  beforeEach(() => {
+    UserInformation.setLoggedIn(false);
+  });
+
+  it('should return false on logged in if user is not logged in', () => {
+    expect(UserInformation.isLoggedIn()).toBe(false);
+  });
+
+  it('should return true on logged in if user is logged in', () => {
+    UserInformation.setLoggedIn(true);
+
+    expect(UserInformation.isLoggedIn()).toBe(true);
+  });
+});
