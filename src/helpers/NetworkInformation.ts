67dc91286cc35bf7087619075b83d60f231b7d70
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-02-20 08:28:15 +0100

    Fix: Fix bug where unknown network crashes app

--- src/helpers/NetworkInformation.ts
+++ src/helpers/NetworkInformation.ts
@@ -1,6 +1,9 @@
+import { LogBuilder } from 'Helpers/LogBuilder';
 import Cookies from 'js-cookie';
 import { NetworkService } from 'Services/NetworkService';
 import { Network } from 'Models/Network';
+
+const Log = LogBuilder.create('Network Information');
 
 export class NetworkInformation {
   static inject = [NetworkService];
@@ -13,18 +16,23 @@ export class NetworkInformation {
   }
 
   fetchNetworks(): Promise<any> {
-    let selectedNetworkEU = Cookies.get('selectedNetworkEUI');
+    let selectedNetworkEUI = Cookies.get('selectedNetworkEUI');
 
     return this.networkService.fetchAllNetworks().then((networks) => {
       this.availableNetworks = networks;
 
-      if (selectedNetworkEU) {
+      Log.debug(selectedNetworkEUI, networks);
+
+      if (selectedNetworkEUI) {
         let selectedNetwork = networks.find((network) => {
-          return network.netEui === selectedNetworkEU;
+          return network.netEui === selectedNetworkEUI;
         });
 
         if (selectedNetwork) {
           this.selectedNetwork = selectedNetwork;
+        } else {
+          Log.warn('Tried to select an nonexisting network. Fallback to first network found');
+          this.selectedNetwork = networks[0];
         }
       } else {
         this.selectedNetwork = networks[0];
