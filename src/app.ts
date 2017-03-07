f8c29d12a5fd7061e16ad6e41a771c3de939b969
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-03-07 09:14:57 +0100

    Feat: Use Connect profile as login indicator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { autoinject } from 'aurelia-framework';
 import { Redirect, Router } from 'aurelia-router';
 
 import { routes } from './appRoutes';
@@ -7,9 +8,11 @@ import { LogBuilder } from 'Helpers/LogBuilder';
 
 const Log = LogBuilder.create();
 
+@autoinject
 export class App {
   constructor(
-    private router: Router
+    private router: Router,
+    private userInformation: UserInformation
   ) { }
 
   getUnknownRoute() {
@@ -25,25 +28,30 @@ export class App {
     };
   }
 
-  configureRouter(config, router) {
+  configureRouter(config) {
     config.title = 'Telenor LORA';
     config.mapUnknownRoutes(this.getUnknownRoute());
-    config.addAuthorizeStep(new AuthorizeStep());
+    config.addAuthorizeStep(new AuthorizeStep(this.userInformation));
     config.map(routes);
-
-    this.router = router;
   }
 }
 
 class AuthorizeStep {
+  constructor(
+    private userInformation: UserInformation
+  ) { }
+
   run(navigationInstruction, next) {
     if (navigationInstruction.getAllInstructions().some(i => i.config.settings.auth)) {
-      Log.debug('Auth protected route', navigationInstruction, !!UserInformation.isLoggedIn());
-      let isLoggedIn = !!UserInformation.isLoggedIn(); // insert magic here;
-      if (!isLoggedIn) {
+      Log.debug('Auth protected route', navigationInstruction, 'Profile', this.userInformation.userProfile);
+
+      return this.userInformation.fetchUserProfile().then(() => {
+        return next();
+      }).catch(() => {
         return next.cancel(new Redirect('login'));
-      }
+      });
+    } else {
+      return next();
     }
-    return next();
   }
 }
