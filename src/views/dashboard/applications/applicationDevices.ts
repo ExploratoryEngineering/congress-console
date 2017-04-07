3a79ff322bc614cf86e8816d2e0b8acd9b6e82a7
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-04-07 09:39:07 +0200

    feat(applicationDetails): add handler for deleting device

--- src/views/dashboard/applications/applicationDevices.ts
+++ src/views/dashboard/applications/applicationDevices.ts
@@ -1,3 +1,4 @@
+import { EventAggregator } from 'aurelia-event-aggregator';
 import { autoinject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { DialogService } from 'aurelia-dialog';
@@ -9,6 +10,7 @@ import { Application } from 'Models/Application';
 import { Device } from 'Models/Device';
 
 import { CreateDeviceDialog } from 'Dialogs/createDeviceDialog';
+import { MessageDialog } from 'Dialogs/messageDialog';
 
 import { LogBuilder } from 'Helpers/LogBuilder';
 const Log = LogBuilder.create('Application devices');
@@ -18,6 +20,7 @@ export class ServiceDetails {
   application: Application = new Application();
   allApplications: Application[] = [];
   selectableApplications: Application[] = [];
+  subscriptions: any[] = [];
 
   devices: Device[] = [];
 
@@ -25,6 +28,7 @@ export class ServiceDetails {
     private applicationService: ApplicationService,
     private deviceService: DeviceService,
     private dialogService: DialogService,
+    private eventAggregator: EventAggregator,
     private router: Router
   ) { }
 
@@ -41,7 +45,32 @@ export class ServiceDetails {
     });
   }
 
+  deleteDevice(device: Device) {
+    this.dialogService.open({
+      viewModel: MessageDialog,
+      model: {
+        messageHeader: 'Delete device?',
+        message: `Are you sure you want to delete the device with EUI: ${device.deviceEUI}`,
+        confirmButtonText: 'Delete',
+        cancelButtonText: 'Cancel'
+      }
+    }).then(response => {
+      if (!response.wasCancelled) {
+        Log.debug('Deleting device');
+        this.deviceService.deleteDevice(this.application.appEUI, device).then(() => {
+          this.devices = this.devices.filter(dev => dev.deviceEUI !== device.deviceEUI);
+        });
+      } else {
+        Log.debug('Did not delete application');
+      }
+    });
+  }
+
   activate(args) {
+    this.subscriptions.push(this.eventAggregator.subscribe('device:delete', (device) => {
+      this.deleteDevice(device);
+    }));
+
     return Promise.all([
       this.applicationService.fetchApplications().then((applications) => {
         this.allApplications = applications;
@@ -67,5 +96,10 @@ export class ServiceDetails {
       Log.error(err);
       this.router.navigate('');
     });
+  }
+
+  deactivate() {
+    this.subscriptions.forEach(subscription => subscription.dispose());
+    this.subscriptions = [];
   }
 }
