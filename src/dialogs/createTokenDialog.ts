f7d9bfdf9b6b85a82b2ee6b193c93625a7d85b42
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-04-18 14:34:13 +0200

    feat(createTokenDialog): add dialog for creating tokens

---
+++ src/dialogs/createTokenDialog.ts
@@ -0,0 +1,42 @@
+import { DialogController } from 'aurelia-dialog';
+import { autoinject } from 'aurelia-framework';
+
+import { TokenService } from 'Services/TokenService';
+
+import { Application } from 'Models/Application';
+import { Token } from 'Models/Token';
+
+const AccessLevels = {
+  readonly: false,
+  fullaccess: true
+};
+
+@autoinject
+export class CreateTokenDialog {
+  token: Token;
+  application: Application;
+
+  selectedAccessLevel: string = 'readonly';
+
+  constructor(
+    private dialogController: DialogController,
+    private tokenService: TokenService
+  ) {
+    this.token = new Token({
+      write: false
+    });
+  }
+
+  createToken() {
+    this.token.resource = this.tokenService.resourcePathForApplication(this.application.appEUI);
+    this.token.write = AccessLevels[this.selectedAccessLevel];
+
+    this.tokenService.createToken(this.token).then(token => {
+      this.dialogController.ok(token);
+    });
+  }
+
+  activate(args) {
+    this.application = args.application;
+  }
+}
