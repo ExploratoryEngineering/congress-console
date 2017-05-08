aae8b33027886047db451f3d828d055b79605b89
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-05-08 17:16:26 +0200

    feat(gateway-card): add gateway-card to gateway view

---
+++ src/components/gateway-card.ts
@@ -0,0 +1,26 @@
+import { Gateway } from 'Models/Gateway';
+import { LogBuilder } from 'Helpers/LogBuilder';
+import { bindable, containerless, autoinject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+import Debug from 'Helpers/Debug';
+
+const Log = LogBuilder.create('Gateway-card');
+
+@containerless
+@autoinject
+export class GatewayCard {
+  @bindable gateway: Gateway;
+
+  constructor(
+    private eventAggregator: EventAggregator
+  ) { }
+
+  editGateway() {
+    this.eventAggregator.publish('gateway:edit', this.gateway);
+  }
+
+  deleteGateway() {
+    this.eventAggregator.publish('gateway:delete', this.gateway);
+  }
+}
