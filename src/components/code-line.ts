38cdd57671295d4f9e6245489c103cc9267c901d
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-05-12 14:21:02 +0200

    refactor(code-line): add snackbar message upon copy

--- src/components/code-line.ts
+++ src/components/code-line.ts
@@ -1,9 +1,11 @@
+import { EventAggregator } from 'aurelia-event-aggregator';
 import { LogBuilder } from 'Helpers/LogBuilder';
-import { bindable, containerless } from 'aurelia-framework';
+import { bindable, containerless, autoinject } from 'aurelia-framework';
 
 const Log = LogBuilder.create('Code line');
 
 @containerless
+@autoinject
 export class CodeLine {
   @bindable
   icon: string;
@@ -11,11 +13,16 @@ export class CodeLine {
   code: string;
   codeTextField: HTMLInputElement;
 
+  constructor(
+    private eventAggregator: EventAggregator
+  ) { }
+
   copySource() {
     Log.debug('Copying code line to clipboard');
     this.codeTextField.select();
     try {
       document.execCommand('copy');
+      this.eventAggregator.publish('global:message', { body: 'Copied code' });
     } catch (error) {
       Log.warn('Copy failed');
     }
