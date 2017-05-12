c9ecb6e4bfdf46d210e46e33f3c6f7429f54b7a9
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-05-12 14:22:21 +0200

    refactor(source-code): add snackbar message upon copy

--- src/components/source-code.ts
+++ src/components/source-code.ts
@@ -1,8 +1,10 @@
-import { bindable } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { bindable, autoinject } from 'aurelia-framework';
 import { LogBuilder } from 'Helpers/LogBuilder';
 
 const Log = LogBuilder.create('Source code');
 
+@autoinject
 export class SourceCode {
   sourceTextField: HTMLTextAreaElement;
   @bindable
@@ -10,9 +12,14 @@ export class SourceCode {
   @bindable
   source: string;
 
+  constructor(
+    private eventAggregator: EventAggregator
+  ) { }
+
   copySource() {
     Log.debug('Copying source code to clipboard');
     this.sourceTextField.select();
+    this.eventAggregator.publish('global:message', { body: 'Copied code' });
     try {
       document.execCommand('copy');
     } catch (error) {
