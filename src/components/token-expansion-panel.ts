eb60a216c86a410d6f5e502442f00056641d444e
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-06-30 13:52:22 +0200

    feat(token-expansion-panel): add dynamic curl commands based on token

--- src/components/token-expansion-panel.ts
+++ src/components/token-expansion-panel.ts
@@ -1,18 +1,32 @@
+import { computedFrom } from 'aurelia-binding';
 import { AureliaConfiguration } from 'aurelia-configuration';
 import { bindable, autoinject, containerless } from 'aurelia-framework';
-import { LogBuilder } from 'Helpers/LogBuilder';
 import { EventAggregator } from 'aurelia-event-aggregator';
+
+import { TagHelper } from 'Helpers/TagHelper';
+import { LogBuilder } from 'Helpers/LogBuilder';
+
+import { Application } from 'Models/Application';
+import { Gateway } from 'Models/Gateway';
 import { Token } from 'Models/Token';
+import { TokenHelper } from 'Helpers/TokenHelper';
 
 const Log = LogBuilder.create('Token expansion panel');
+
+const th = new TagHelper();
+const tokenHelper = new TokenHelper();
 
 @autoinject
 @containerless
 export class TokenExpansionPanel {
   @bindable
-  token: Token;
+  token: Token = new Token();
   @bindable
   active: boolean = false;
+  @bindable
+  applications: Application[] = [];
+  @bindable
+  gateways: Gateway[] = [];
 
   constructor(
     private eventAggregator: EventAggregator,
@@ -23,7 +37,80 @@ export class TokenExpansionPanel {
     this.active = !this.active;
   }
 
-  curlGetDevices() {
+  @computedFrom('token', 'applications', 'gateways')
+  get description() {
+    if (tokenHelper.isTokenDangling(this.token, { applications: this.applications, gateways: this.gateways })) {
+      return 'This resource is not connected to any resource. It can safely be deleted.';
+    }
+    return tokenHelper.getDescription(this.token);
+  }
+
+  @computedFrom('token')
+  get hasFullAccessToAllResources(): boolean {
+    return tokenHelper.getTokenRights(this.token).resource === 'all';
+  }
+
+  @computedFrom('token')
+  get hasAccessToAllGateways(): boolean {
+    const tokenRights = tokenHelper.getTokenRights(this.token);
+    return tokenRights.resource === 'all' || (tokenRights.resource === 'gateways' && tokenRights.resourceId === 'all');
+  }
+
+  @computedFrom('token')
+  get hasAccessAllApplications(): boolean {
+    const tokenRights = tokenHelper.getTokenRights(this.token);
+    return tokenRights.resource === 'all' || (tokenRights.resource === 'applications' && tokenRights.resourceId === 'all');
+  }
+
+  @computedFrom('token')
+  get hasAccessToSpecificApplication(): boolean {
+    const tokenRights = tokenHelper.getTokenRights(this.token);
+    return tokenRights.resource === 'applications' && tokenRights.resourceId !== 'all';
+  }
+
+  @computedFrom('token')
+  get hasAccessToSpecificGateway(): boolean {
+    const tokenRights = tokenHelper.getTokenRights(this.token);
+    return tokenRights.resource === 'gateways' && tokenRights.resourceId !== 'all';
+  }
+
+  curlGetRoot() {
+    return `curl
+    -XGET ${this.config.get('api.endpoint')}/
+    -HX-API-Token:${this.token.token}`;
+  }
+
+  curlGetTokens() {
+    return `curl
+    -XGET ${this.config.get('api.endpoint')}/tokens
+    -HX-API-Token:${this.token.token}`;
+  }
+
+  curlGetApplications() {
+    return `curl
+    -XGET ${this.config.get('api.endpoint')}/applications
+    -HX-API-Token:${this.token.token}`;
+  }
+
+  curlGetGateways() {
+    return `curl
+    -XGET ${this.config.get('api.endpoint')}/gateways
+    -HX-API-Token:${this.token.token}`;
+  }
+
+  curlGetGatewayInformation() {
+    return `curl
+    -XGET ${this.config.get('api.endpoint')}${this.token.resource}
+    -HX-API-Token:${this.token.token}`;
+  }
+
+  curlGetApplicationInformation() {
+    return `curl
+    -XGET ${this.config.get('api.endpoint')}${this.token.resource}
+    -HX-API-Token:${this.token.token}`;
+  }
+
+  curlGetApplicationDevices() {
     return `curl
     -XGET ${this.config.get('api.endpoint')}${this.token.resource}/devices
     -HX-API-Token:${this.token.token}`;
