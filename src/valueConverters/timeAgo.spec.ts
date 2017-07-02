89abb0245c4760bb767a2d824487bcd47cb66fcc
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-07-02 16:19:07 +0200

    test(timeAgo): add tests for timeAgo value converter

---
+++ src/valueConverters/timeAgo.spec.ts
@@ -0,0 +1,18 @@
+import { TimeAgoValueConverter } from './timeAgo';
+
+Date.now = jest.fn(() => 1490997600000);
+
+const timeAgo = new TimeAgoValueConverter();
+
+describe('TimeAgo value converter', () => {
+  const firstOfApril = '1490997600';
+  const illegalDate = 'abc';
+
+  it('should return the default format if given correct date but no format', () => {
+    expect(timeAgo.toView(firstOfApril)).toBe('a few seconds ago');
+  });
+
+  it('should show a error message if the given value is illegal', () => {
+    expect(timeAgo.toView(illegalDate)).toBe('Invalid date');
+  });
+});
