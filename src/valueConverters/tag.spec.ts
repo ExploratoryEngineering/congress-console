7265185b55e3b80de48c3d0305458df8a8ec0b1c
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-07-02 16:13:11 +0200

    test(tag): add tests to tag valueConverter

---
+++ src/valueConverters/tag.spec.ts
@@ -0,0 +1,32 @@
+import { TagValueConverter } from './tag';
+
+const tagValueConverter = new TagValueConverter();
+
+describe('Tag valueconverter', () => {
+  it('should correctly render a tag to view', () => {
+    const tag: Tag = {
+      key: 'testKey',
+      value: 'testValue'
+    };
+
+    expect(tagValueConverter.toView(tag)).toBe('testKey:testValue');
+  });
+
+  it('should correctly render a tag from view', () => {
+    const tagAsText = 'testKey:testValue';
+
+    expect(tagValueConverter.fromView(tagAsText)).toEqual({
+      key: 'testKey',
+      value: 'testValue'
+    });
+  });
+
+  it('should correctly render a tag to and from a view with the same result', () => {
+    const tag: Tag = {
+      key: 'testKey',
+      value: 'testValue'
+    };
+
+    expect(tagValueConverter.fromView(tagValueConverter.toView(tag))).toEqual(tag);
+  });
+});
