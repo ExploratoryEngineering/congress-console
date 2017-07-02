70d20275c45a90c06ed7ccb0cffbb07ff5a637bf
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-07-02 14:36:22 +0200

    chore(main): remove old comments and readme

--- src/main.ts
+++ src/main.ts
@@ -34,9 +34,6 @@ export async function configure(aurelia: Aurelia) {
       }
     });
 
-  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  // aurelia.use.plugin('aurelia-html-import-template-loader')
-
   await aurelia.start();
   aurelia.setRoot(PLATFORM.moduleName('app'));
 
@@ -50,11 +47,4 @@ export async function configure(aurelia: Aurelia) {
       }
     }, 450);
   });
-
-  // if you would like your website to work offline (Service Worker),
-  // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
-  /*
-  const offline = await System.import('offline-plugin/runtime');
-  offline.install();
-  */
 }
