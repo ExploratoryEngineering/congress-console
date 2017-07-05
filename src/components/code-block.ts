2a1bbc960f19acb5cc65afd252a75050e3e0df39
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-07-05 16:33:55 +0200

    feat(code-block): add new component for code-block

---
+++ src/components/code-block.ts
@@ -0,0 +1,33 @@
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { LogBuilder } from 'Helpers/LogBuilder';
+import { bindable, containerless, autoinject } from 'aurelia-framework';
+
+const Log = LogBuilder.create('Code line');
+
+@containerless
+@autoinject
+export class CodeBlock {
+  @bindable
+  icon: string;
+  @bindable
+  code: string;
+  @bindable
+  heading: string;
+
+  codeTextField: HTMLInputElement;
+
+  constructor(
+    private eventAggregator: EventAggregator
+  ) { }
+
+  copySource() {
+    Log.debug('Copying code line to clipboard');
+    this.codeTextField.select();
+    try {
+      document.execCommand('copy');
+      this.eventAggregator.publish('global:message', { body: 'Copied code' });
+    } catch (error) {
+      Log.warn('Copy failed');
+    }
+  }
+}
