a318357fa9f2f66c2ebffbb7bd89388161aa0dc4
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-07-21 09:51:06 +0200

    refactor(tagName): add warn log

--- src/valueConverters/tagName.ts
+++ src/valueConverters/tagName.ts
@@ -1,6 +1,8 @@
+import { LogBuilder } from 'Helpers/LogBuilder';
 import { TagHelper } from 'Helpers/TagHelper';
 
 const th = new TagHelper();
+const Log = LogBuilder.create('Tag name');
 
 /**
  * Simple value converter to map from a potential name in tag with fallback
@@ -8,6 +10,7 @@ const th = new TagHelper();
 export class TagNameValueConverter {
   toView(entity: TagEntity, fallbackKey: string) {
     if (!entity) {
+      Log.warn('Tried to find name of falsey value');
       return '';
     }
     return th.getEntityName(entity, fallbackKey);
