8c02e6dda6e1df70d34135f8b0d8d3f9859d2924
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-07-29 11:12:33 +0200

    refactor(loadingIndicator): override default colors

--- src/components/loading-indicator.ts
+++ src/components/loading-indicator.ts
@@ -1,6 +1,7 @@
 import { bindable, noView } from 'aurelia-framework';
 
 import '../../node_modules/nprogress/nprogress.css';
+import './loading-indicator.scss';
 import * as NProgress from 'nprogress';
 
 import { LogBuilder } from 'Helpers/LogBuilder';
