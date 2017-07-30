59e54b93af5a5bb9132e93f295885f146be5086f
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-07-30 09:44:52 +0200

    chore(application-details-card): move scss import to ts instead of html due to hmr

--- src/components/application-details-card.ts
+++ src/components/application-details-card.ts
@@ -2,6 +2,8 @@ import { Application } from 'Models/Application';
 import { EventAggregator } from 'aurelia-event-aggregator';
 import { autoinject, bindable } from 'aurelia-framework';
 
+import './application-details-card.scss';
+
 @autoinject
 export class ApplicationDetailsCard {
   @bindable
