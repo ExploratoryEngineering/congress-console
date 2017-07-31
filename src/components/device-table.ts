f86437322acf4a8d704e33e127bd6293b21ee268
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-07-31 11:43:53 +0200

    refactor(device-table): add dropdown and incapsulate deleting of devices

---
+++ src/components/device-table.ts
@@ -0,0 +1,51 @@
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { DialogService } from 'aurelia-dialog';
+import { bindable, autoinject } from 'aurelia-framework';
+
+import { DeviceService } from 'Services/DeviceService';
+
+import { Device } from 'Models/Device';
+
+import { MessageDialog } from 'Dialogs/messageDialog';
+
+import { LogBuilder } from 'Helpers/LogBuilder';
+
+const Log = LogBuilder.create('Device table');
+
+@autoinject
+export class DeviceTable {
+  @bindable
+  devices: Device[];
+  @bindable
+  applicationEui: string;
+
+  constructor(
+    private dialogService: DialogService,
+    private deviceService: DeviceService,
+    private eventAggregator: EventAggregator
+  ) { }
+
+  deleteDevice(device: Device) {
+    this.dialogService.open({
+      viewModel: MessageDialog,
+      model: {
+        messageHeader: 'Delete device?',
+        message: `Are you sure you want to delete the device with EUI: ${device.deviceEUI}`,
+        confirmButtonText: 'Delete',
+        cancelButtonText: 'Cancel'
+      }
+    }).whenClosed(response => {
+      if (!response.wasCancelled) {
+        Log.debug('Deleting device');
+        this.deviceService.deleteDevice(this.applicationEui, device).then(() => {
+          this.eventAggregator.publish('global:message', {
+            body: 'Device deleted'
+          });
+          this.devices = this.devices.filter(dev => dev.deviceEUI !== device.deviceEUI);
+        });
+      } else {
+        Log.debug('Did not delete device');
+      }
+    });
+  }
+}
