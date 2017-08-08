c4936f50dd4eb293a1b2e6a04d275e8fe89675f5
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-08-08 18:05:53 +0200

    test(appRoutes): add tests according to bug with missing route

---
+++ src/appRoutes.spec.ts
@@ -0,0 +1,21 @@
+import { routes as appRoutes } from './appRoutes';
+
+const includesRouteName = (routeName) => {
+  return appRoutes.some((routeConfig) => {
+    return routeConfig.route.includes(routeName);
+  });
+};
+
+describe('Application routes', () => {
+  it('should have a server error route', () => expect(includesRouteName('server-error')).toBe(true));
+  it('should have a not found route', () => expect(includesRouteName('not-found')).toBe(true));
+  it('should have a login route', () => expect(includesRouteName('login')).toBe(true));
+  it('should have a dashboard route', () => expect(includesRouteName('dashboard')).toBe(true));
+  it('should have a gateway route', () => expect(includesRouteName('gateway-dashboard')).toBe(true));
+  it('should have a api key route', () => expect(includesRouteName('api-keys')).toBe(true));
+  it('should have a default redirect to dashobard', () => {
+    expect(appRoutes.some((routeConfig) => {
+      return routeConfig.route.includes('') && routeConfig.redirect === 'dashboard';
+    })).toBe(true);
+  });
+});
