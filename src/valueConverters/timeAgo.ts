a894bff3305875cd74daaa58432f6101fbc2c106
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-09-06 16:23:14 +0200

    refactor(timeAgo): add log warn when invalid date

--- src/valueConverters/timeAgo.ts
+++ src/valueConverters/timeAgo.ts
@@ -1,6 +1,9 @@
+import { LogBuilder } from 'Helpers/LogBuilder';
 import * as moment from 'moment';
 
 const INVALID_DATE = 'Invalid date';
+
+const Log = LogBuilder.create('Time ago');
 
 export class TimeAgoValueConverter {
   toView(value) {
@@ -9,6 +12,7 @@ export class TimeAgoValueConverter {
     if (date.isValid()) {
       return date.fromNow();
     } else {
+      Log.warn('Invalid date', value);
       return INVALID_DATE;
     }
   }
