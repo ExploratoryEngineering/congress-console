1b62f2ddf7c78c3eb2e3e341ce2241f9b8a9f741
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-09-18 13:15:07 +0200

    feat(applicationIntegrations): add output-table

--- src/views/dashboard/applications/applicationIntegrations.ts
+++ src/views/dashboard/applications/applicationIntegrations.ts
@@ -1,23 +1,49 @@
+
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { DialogService } from 'aurelia-dialog';
 import { computedFrom } from 'aurelia-binding';
-import { autoinject } from 'aurelia-framework';
+import { autoinject, PLATFORM } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 
-import { ApplicationService } from 'Services/ApplicationService';
+import { Output } from 'Models/Output';
+import { OutputService } from 'Services/OutputService';
 import { Application } from 'Models/Application';
+import { ApplicationService } from 'Services/ApplicationService';
 
 import { LogBuilder } from 'Helpers/LogBuilder';
 const Log = LogBuilder.create('Application devices');
 
 @autoinject
 export class ServiceDetails {
+  outputs: Output[] = [];
+
   application: Application = new Application();
   allApplications: Application[] = [];
   selectableApplications: Application[] = [];
 
   constructor(
     private applicationService: ApplicationService,
+    private outputService: OutputService,
+    private dialogService: DialogService,
+    private eventAggregator: EventAggregator,
     private router: Router
   ) { }
+
+  createNewOutput() {
+    this.dialogService.open({
+      viewModel: PLATFORM.moduleName('dialogs/createOutputDialog'),
+      model: {
+        applicationEui: this.application.appEUI
+      }
+    }).whenClosed(response => {
+      if (!response.wasCancelled) {
+        this.eventAggregator.publish('global:message', {
+          body: 'Output created'
+        });
+        this.outputs.push(response.output);
+      }
+    });
+  }
 
   @computedFrom('application')
   get wstaCodeExample() {
@@ -51,6 +77,10 @@ export class ServiceDetails {
 
         this.selectableApplications = this.allApplications.filter((application) => {
           return application.appEUI !== this.application.appEUI;
+        });
+      }).then(() => {
+        this.outputService.getOutputsForApplication(this.application.appEUI).then(outputs => {
+          this.outputs = outputs;
         });
       })
     ]).catch(err => {
