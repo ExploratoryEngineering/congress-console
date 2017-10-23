875596c52bfcbfd8357296c6a6cadad482a3021b
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-10-23 16:17:52 +0200

    feat(pagination): add new component for pagination

---
+++ src/components/common/pagination/pagination.spec.ts
@@ -0,0 +1,140 @@
+import { Pagination } from './pagination';
+
+const arrayWith10 = new Array(10).fill('test');
+const arrayWith3 = new Array(3).fill('test');
+
+describe('Pagination', () => {
+  let pagination: Pagination;
+  beforeEach(() => {
+    pagination = new Pagination();
+    pagination.filterCallback = () => { };
+    pagination.rowsPrPage = '5';
+  });
+
+  describe('basic functionality', () => {
+    it('should correctly filter correct amount of devices based on ', () => {
+      pagination.collection = arrayWith10;
+      pagination.filter();
+
+      expect(pagination.filteredCollection.length).toBe(5);
+    });
+
+    it('should call filter upon bind', () => {
+      let spy = spyOn(pagination, 'filter');
+      pagination.bind();
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should call filterCallback upon filter', () => {
+      let spy = spyOn(pagination, 'filterCallback');
+      pagination.filter();
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should show correct fromRange', () => {
+      pagination.collection = arrayWith10;
+      pagination.filter();
+
+      expect(pagination.getFromRange()).toBe(1);
+    });
+
+    it('should show correct fromRange when paginated', () => {
+      pagination.collection = arrayWith10;
+      pagination.offset = 1;
+      pagination.filter();
+
+      expect(pagination.getFromRange()).toBe(6);
+    });
+
+    it('should show correct toRange', () => {
+      pagination.collection = arrayWith10;
+      pagination.filter();
+
+      expect(pagination.getToRange()).toBe(5);
+    });
+
+    it('should show correct toRange when paginated', () => {
+      pagination.collection = arrayWith10;
+      pagination.offset = 1;
+      pagination.filter();
+
+      expect(pagination.getToRange()).toBe(10);
+    });
+
+    it('shoudl show correct toRange when limited data', () => {
+      pagination.collection = arrayWith3;
+      pagination.filter();
+
+      expect(pagination.getToRange()).toBe(3);
+    });
+  });
+
+  describe('previous/next', () => {
+    it('should correctly allow for going to next page', () => {
+      pagination.collection = arrayWith10;
+      pagination.filter();
+
+      expect(pagination.offset).toBe(0);
+      pagination.increasePageNumber();
+      expect(pagination.offset).toBe(1);
+    });
+
+    it('should correctly allow for going to previous page', () => {
+      pagination.collection = arrayWith10;
+      pagination.offset = 1;
+      pagination.filter();
+
+      expect(pagination.offset).toBe(1);
+      pagination.decreasePageNumber();
+      expect(pagination.offset).toBe(0);
+    });
+
+    it('should not allow for going to next page if there is no data', () => {
+      pagination.rowsPrPage = '10';
+      pagination.collection = arrayWith10;
+      pagination.filter();
+      expect(pagination.offset).toBe(0);
+      pagination.increasePageNumber();
+      expect(pagination.offset).toBe(0);
+    });
+
+    it('should correctly allow for going to previous page', () => {
+      pagination.collection = arrayWith10;
+      pagination.offset = 0;
+      pagination.filter();
+
+      expect(pagination.offset).toBe(0);
+      pagination.decreasePageNumber();
+      expect(pagination.offset).toBe(0);
+    });
+  });
+
+  describe('on changed', () => {
+    it('should call filter upon collectionChanged', () => {
+      let spy = spyOn(pagination, 'filter');
+      pagination.collectionChanged();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  it('should call filter upon rowsPrPageChanged', () => {
+    let spy = spyOn(pagination, 'filter');
+    pagination.rowsPrPageChanged();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should reset offset upon rowsPrPageChanged', () => {
+    pagination.offset = 1;
+    pagination.rowsPrPageChanged();
+
+    expect(pagination.offset).toBe(0);
+  });
+
+  it('should call filter upon offsetChanged', () => {
+    let spy = spyOn(pagination, 'filter');
+    pagination.offsetChanged();
+    expect(spy).toHaveBeenCalled();
+  });
+});
---
+++ src/components/common/pagination/pagination.ts
@@ -0,0 +1,87 @@
+import { computedFrom } from 'aurelia-binding';
+import { bindable, bindingMode } from 'aurelia-framework';
+
+import { LogBuilder } from 'Helpers/LogBuilder';
+
+const Log = LogBuilder.create('Pagination');
+
+export class Pagination {
+  @bindable
+  collection: any[] = [];
+  @bindable
+  rowsPrPage: string = '10';
+  availableRowsPrPage = [
+    10,
+    25,
+    100
+  ];
+  @bindable
+  offset: number = 0;
+  filteredCollection: any[] = [];
+
+  @bindable
+  filterCallback;
+
+  filter() {
+    let collection = [...this.collection];
+    Log.debug('filtering');
+    this.filteredCollection = collection.splice(parseInt(this.rowsPrPage, 10) * this.offset, parseInt(this.rowsPrPage, 10));
+    this.filterCallback({
+      filteredCollection: this.filteredCollection
+    });
+  }
+
+  /**
+   * Set page number for collection
+   * @param pageNumber Page number 0-indexed
+   */
+  setPageNumber(pageNumber: number) {
+    this.offset = pageNumber;
+  }
+
+  increasePageNumber() {
+    Log.debug('Increase page number', (parseInt(this.rowsPrPage, 10) * (this.offset + 1)));
+    if (this.collection.length - (parseInt(this.rowsPrPage, 10) * (this.offset + 1)) > 0) {
+      this.offset += 1;
+    }
+  }
+
+  decreasePageNumber() {
+    Log.debug('Decrease page number');
+    if (this.offset - 1 >= 0) {
+      this.offset -= 1;
+    }
+  }
+
+  setRowsPrPage(rowsPrPage: string) {
+    this.rowsPrPage = rowsPrPage;
+  }
+
+  @computedFrom('filteredCollection')
+  get rowData(): string {
+    return `${this.getFromRange()} - ${this.getToRange()} of ${this.collection.length}`;
+  }
+
+  getFromRange(): number {
+    return (this.offset * parseInt(this.rowsPrPage, 10) + 1);
+  }
+
+  getToRange(): number {
+    return (this.offset * parseInt(this.rowsPrPage, 10)) + this.filteredCollection.length;
+  }
+
+  bind() {
+    this.filter();
+  }
+
+  collectionChanged() {
+    this.filter();
+  }
+  rowsPrPageChanged() {
+    this.offset = 0;
+    this.filter();
+  }
+  offsetChanged() {
+    this.filter();
+  }
+}
