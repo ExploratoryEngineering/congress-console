95d930d0b97a627c563ff085c5f3bb9f86847b57
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-10-27 15:27:54 +0200

    feat(secret-text): add component for rendering secret text

---
+++ src/components/common/secret-text/secret-text.ts
@@ -0,0 +1,14 @@
+import { bindable } from 'aurelia-framework';
+
+export class SecretText {
+  @bindable
+  secretText: string = '';
+  @bindable
+  visible: boolean = false;
+  @bindable
+  placeholderText: string = '<secret>';
+
+  toggleVisibility() {
+    this.visible = !this.visible;
+  }
+}
