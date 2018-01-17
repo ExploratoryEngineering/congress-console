1698cdd707f4840b65cc0545d69bbc6f52e2f220
Per Kristian Kummermo <per.kristian@telenordigital.com>
2018-01-17 16:16:58 +0100

    refactor(deploy): rename to prod deploy and add cloudfront invalidation

--- scripts/deployToProd.sh
+++ scripts/deployToProd.sh
@@ -7,7 +7,7 @@ aws s3 sync dist/ s3://lora.engineering/ --dryrun --acl public-read --profile s3
 while true; do
     read -p "Does this look ok? [Yy/Nn]" yn
     case $yn in
-        [Yy]* ) aws s3 sync dist/ s3://lora.engineering/ --acl public-read --profile s3-lora-web-admin --delete; break;;
+        [Yy]* ) aws s3 sync dist/ s3://lora.engineering/ --acl public-read --profile s3-lora-web-admin --delete; aws cloudfront create-invalidation --distribution-id E3BA2B249FQAKT --paths "/*"; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
