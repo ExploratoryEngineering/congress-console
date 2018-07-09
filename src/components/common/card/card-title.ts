62011990fbfca36ac5bc7ae6435edd39848c94c5
Per Kristian Kummermo <per.kristian@telenordigital.com>
2018-07-09 08:30:20 +0200

    chore(card-title): remove unused code

--- src/components/common/card/card-title.ts
+++ src/components/common/card/card-title.ts
@@ -1,13 +1,5 @@
-import { bindable, containerless } from "aurelia-framework";
+import { containerless } from "aurelia-framework";
 
 @containerless
 export class CardTitle {
-  @bindable
-  warning: boolean = false;
-  @bindable
-  table: string;
-
-  useTableSpacing() {
-    return this.table || this.table === "";
-  }
 }
